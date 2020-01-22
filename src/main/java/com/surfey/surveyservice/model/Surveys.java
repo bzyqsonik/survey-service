0cda5f0b155439690b914ce10f2216a08a4e2ff0
bzyqsonik <muszynski94@tlen.pl>
2020-01-22 16:57:35 +0100

    Update Surveys.java

--- src/main/java/com/surfey/surveyservice/model/Surveys.java
+++ src/main/java/com/surfey/surveyservice/model/Surveys.java
@@ -6,6 +6,7 @@ import javax.validation.constraints.NotNull;
 import java.util.List;
 import java.util.UUID;
 
+// Osobne linie + static import i NonNull z lomboka na liscie
 @Value @Builder @AllArgsConstructor(access = AccessLevel.PRIVATE) @NoArgsConstructor(force = true, access = AccessLevel.PRIVATE)
 public class Surveys {
     @NotNull
