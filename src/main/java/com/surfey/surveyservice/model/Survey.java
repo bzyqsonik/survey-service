4b44c0d2d29fa818128d85bca0d0f245f813acc8
bzyqsonik <muszynski94@tlen.pl>
2020-01-22 16:57:01 +0100

    Update Survey.java

--- src/main/java/com/surfey/surveyservice/model/Survey.java
+++ src/main/java/com/surfey/surveyservice/model/Survey.java
@@ -6,10 +6,13 @@ import javax.validation.constraints.NotNull;
 import java.util.List;
 import java.util.UUID;
 
+//Kazda adnotacja do osobnej linii + static importy na PRIVATE
 @Value @Builder @AllArgsConstructor(access = AccessLevel.PRIVATE) @NoArgsConstructor(force = true, access = AccessLevel.PRIVATE)
 public class Survey {
+    //@NonNull
     UUID uuid;
-    @NotNull
+    @NotNull //@NonNull lombok
     List<Question> questions;
+    //@NonNull
     String name;
 }
