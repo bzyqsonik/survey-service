e906b8b507e21259eabe91007a58706cdee7c7d5
bzyqsonik <muszynski94@tlen.pl>
2020-01-22 16:33:10 +0100

    Update SurveyController.java

--- src/main/java/com/surfey/surveyservice/controller/SurveyController.java
+++ src/main/java/com/surfey/surveyservice/controller/SurveyController.java
@@ -12,23 +12,29 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/surveys")
 @AllArgsConstructor
-@Value // makes all fields private final
+@Value // USUN TO NA BOGA
 public class SurveyController {
 
+    // Wyjeb @Value i zrob z tego private final
     SurveyService surveyService;
 
     /**
      * @param loadRelated Determines whether all the surveys from data source
      *                    are retrieved with all related questions or with UIDs of those.
      * @return JSON object of all surveys in the data source
-     */
+     */ 
+    //JEBAC JAVA DOCI
 
     @GetMapping
+    //Ta flaga jest chujowa, nie powinno sie tak sterowac odpowiedzia. Jak juz to powinienes miec 2 osobne endpointy i 2 modele danych
+    //Wszystkie zmienne, argumenty metod TEZ powinny byc final
     public Surveys getSurveys(@RequestParam(required = false) boolean loadRelated) {
         return surveyService.findAll(loadRelated);
     }
 
     @GetMapping("/{surveyId}")
+    //Ta flaga jest chujowa, nie powinno sie tak sterowac odpowiedzia. Jak juz to powinienes miec 2 osobne endpointy i 2 modele danych
+    //Wszystkie zmienne, argumenty metod TEZ powinny byc final
     public Survey findSurvey(@PathVariable UUID surveyId, @RequestParam(required = false) boolean loadRelated) {
         return surveyService.find(surveyId, loadRelated);
     }
