592ed3bff0efec86308b65ce5472750559e0d7a5
bzyqsonik <muszynski94@tlen.pl>
2020-01-22 17:03:46 +0100

    Update QuestionEntity.java

--- src/main/java/com/surfey/surveyservice/entity/QuestionEntity.java
+++ src/main/java/com/surfey/surveyservice/entity/QuestionEntity.java
@@ -17,16 +17,27 @@ import java.util.UUID;
 @NoArgsConstructor
 @Entity
 @Table(name = "Question")
+//@Data
+//@Builder
+//@NoArgsConstructor(access = PRIVATE, force = true)
+//@AllArgsConstructor(access = PRIVATE)
+//@Entity(table(?) = "questions")
+// tabele nie nazywaja sie duza litera i powinny byc w liczbie mnogiej
 public class QuestionEntity {
     @Id
-    @GeneratedValue
+    @GeneratedValue //strategy = IDENTITY
     private int id;
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE) //To nie jest generated value
+    //@NonNull
+    // final
     private UUID uuid;
     @NonNull
-    @OneToMany(cascade = CascadeType.ALL)
-    @Fetch(FetchMode.SUBSELECT)
-    @JoinColumn(name = "questionId")
+    @OneToMany(cascade = CascadeType.ALL) //static import
+    @Fetch(FetchMode.SUBSELECT) // static import
+    @JoinColumn(name = "questionId") //question_id, na bazie nie piszemy camel casem
+    //final
     private List<AnswerEntity> answers;
+    //to raczej tez final, chyba nie bedziemy tego updatowac
+    // @NonNull
     private String content;
 }
