6392381aa3ff62b7c61d0ccf02136f4f74b4490a
bzyqsonik <muszynski94@tlen.pl>
2020-01-22 16:50:40 +0100

    Update SurveyConverter.java

--- src/main/java/com/surfey/surveyservice/service/converter/SurveyConverter.java
+++ src/main/java/com/surfey/surveyservice/service/converter/SurveyConverter.java
@@ -9,7 +9,9 @@ import org.springframework.stereotype.Component;
 import java.util.stream.Collectors;
 
 @Component
+// Ja bym to nazwal mapper bardziej niz converter, bo to co tu robisz to mapowani
 public class SurveyConverter {
+    //final arg
     public Survey convertEntityToSurveyWithoutRelated(SurveyEntity surveyEntity) {
         return Survey.builder()
                 .name(surveyEntity.getName())
@@ -17,20 +19,27 @@ public class SurveyConverter {
                 .build();
     }
 
+    //final arg
     public Survey convertEntityToSurveyWithRelated(SurveyEntity surveyEntity) {
+        // name do nastepnej linii
         return Survey.builder().name(surveyEntity.getName())
                 .uuid(surveyEntity.getUuid())
                 .questions(surveyEntity.getQuestions()
                         .stream()
+                           //mapowanie questionEntity -> question wyjeb do osobnej metody i uzyj tu this::toQuestion
                         .map(questionEntity -> Question.builder()
                                 .uuid(questionEntity.getUuid())
                                 .answers(questionEntity.getAnswers()
                                         .stream()
+                                         //mapowanie answerEntity -> answer wyjeb do osobnej metody i uzyj tu this::toAnswer
                                         .map(answerEntity -> Answer.builder()
                                                 .content(answerEntity.getAnswer())
+                                             //collect do nowej linii
+                                             //static import na toList()
                                                 .uuid(answerEntity.getUuid()).build()).collect(Collectors.toList()))
                                 .content(questionEntity.getContent())
                                 .build())
+                           //static import
                         .collect(Collectors.toList()))
                 .build();
     }
