80065ece614c2124588b7c9b11158324f48e8b33
bzyqsonik <muszynski94@tlen.pl>
2020-01-22 16:54:33 +0100

    Update Answer.java

--- src/main/java/com/surfey/surveyservice/model/Answer.java
+++ src/main/java/com/surfey/surveyservice/model/Answer.java
@@ -6,9 +6,13 @@ import java.util.UUID;
 
 @Value
 @Builder
+// Static import na PRIVATE
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
+// Static import na PRIVATE
 @NoArgsConstructor(force = true, access = AccessLevel.PRIVATE)
 public class Answer {
+    // @NonNull
     UUID uuid;
+    // @NonNull
     String content;
 }
