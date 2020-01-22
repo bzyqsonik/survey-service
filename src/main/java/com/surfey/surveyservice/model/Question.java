ff59c0caa42f66c63e5890487b1964d1772156ab
bzyqsonik <muszynski94@tlen.pl>
2020-01-22 16:55:23 +0100

    Update Question.java

--- src/main/java/com/surfey/surveyservice/model/Question.java
+++ src/main/java/com/surfey/surveyservice/model/Question.java
@@ -6,10 +6,14 @@ import javax.validation.constraints.NotNull;
 import java.util.List;
 import java.util.UUID;
 
+// Static import na PRIVATE
+// Kazda adnotacja powinna byc w osobnej linii
 @Value @Builder @AllArgsConstructor(access = AccessLevel.PRIVATE) @NoArgsConstructor(force = true, access = AccessLevel.PRIVATE)
 public class Question {
+    // @NonNull
     UUID uuid;
+    // @NonNull
     String content;
-    @NotNull
+    @NotNull // @NonNull - uzywaj lombokowych adnotacji
     List<Answer> answers;
 }
